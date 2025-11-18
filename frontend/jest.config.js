110ee05dbb32d2be6aee76c4bcb4d5501103f7c3
yuki yamada <yukikame1021@gmail.com>
2025-11-17 17:59:55 -0800

    Add CI/CD configuration with GitHub Actions

---
+++ frontend/jest.config.js
@@ -0,0 +1,26 @@
+// jest.config.js
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // tsでもOK
+  testEnvironment: "jest-environment-jsdom",
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[jt]s?(x)",
+    "**/*.test.[jt]s?(x)",
+  ],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  collectCoverageFrom: [
+    "app/**/*.{js,jsx,ts,tsx}",
+    "components/**/*.{js,jsx,ts,tsx}",
+    "lib/**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/__tests__/**",
+  ],
+};
+
+module.exports = createJestConfig(customJestConfig);
